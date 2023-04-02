65b58b875642fa2766b6837c6e4ec301a41af13c
joydip <joydip@pop-os.localdomain>
2023-04-02 14:43:46 +0530

    add the auto generate sluge

--- AR_django/base/models.py
+++ AR_django/base/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -24,6 +24,20 @@ class Gallery(models.Model):
             return 'http://127.0.0.1:8000'+self.image.url
         return ''
 
+    def _get_unique_slug(self):
+        slug = slugify(self.title)
+        unique_slug = slug
+        num = 1
+        while Gallery.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+        super().save(*args, **kwargs)
+
 
 class Notice(models.Model):
     topic = models.CharField(max_length=255)
@@ -36,6 +50,20 @@ class Notice(models.Model):
 
     def __str__(self):
         return self.topic
+
+    def _get_unique_slug(self):
+        slug = slugify(self.title)
+        unique_slug = slug
+        num = 1
+        while Notice.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+        super().save(*args, **kwargs)
 
 
 class pojects(models.Model):
