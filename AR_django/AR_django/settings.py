ecf118039351ed172084cdefbd07ab9bb1990af0
cion247 <xocionrocks247@gmail.com>
2023-04-02 12:43:50 +0530

    ssd

--- AR_django/AR_django/settings.py
+++ AR_django/AR_django/settings.py
@@ -55,9 +55,12 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+<<<<<<< HEAD
+=======
+    # 'coarsheaders.middleware.CorsMiddleware',
+>>>>>>> parent of 626e052 (add cross header)
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
