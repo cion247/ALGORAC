c32a1086fac4d288b6a4c4fd47ae3c1fa5180fe2
Bishakha1589 <126696035+Bishakha1589@users.noreply.github.com>
2023-04-03 23:52:16 +0530

    login logout
    login logout

--- AR_django/base/views.py
+++ AR_django/base/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
 from .models import Gallery, Notice, projects
 from .serializers import GallerySerializer, NoticeSerializer, ProjectSerializer, MessagesSerializer
 # Create your views here.
