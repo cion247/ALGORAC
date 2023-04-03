1eecd5e199cb3ab72e478a8cfd44bd7042dffa3f
cion247 <96902253+cion247@users.noreply.github.com>
2023-04-03 18:58:33 +0530

    Delete AR_django directory

--- AR_django/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from base.views import LatestNoticelistView, NoticedetaleView, LatestGallerylistView
-
-urlpatterns = [
-    path('notices-latest/', LatestNoticelistView.as_view()),
-    path('notices-detals/<slug:notice_slug>', NoticedetaleView.as_view()),
-    path('gallery-latest/', LatestGallerylistView.as_view()),
-]
