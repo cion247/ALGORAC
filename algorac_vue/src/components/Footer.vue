1b7b3a6130a969c30a54adf707623bd9a640b37a
swapnaaacodes <126751627+swapnaaacodes@users.noreply.github.com>
2023-04-05 02:32:56 +0530

    footer

---
+++ algorac_vue/src/components/Footer.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="w-full bg-black text-gray-100">
+        <div
+           class="
+               container
+               mx-auto
+               max-w-4xl
+               h-full
+               flex
+               flex-wrap
+               justify-between
+               items-start
+               text-sm
+               p-6
+               pt-8
+               pb-4
+           "
+        >
+            <div class="mb-4">
+                <div class="mb-4">
+                    CONNECT WITH US ON
+                </div>
+                
+            </div>
+            <div class="mb-4">
+                <div class="mb-4">
+                    ALGORAC
+                </div>
+                <p class="mb-4 leading-normal">
+                    ICFAI TRIPURA UNIVERSITY<br>
+                    Kamalghat, Mohanpur<br>
+                    Agartala, West Tripura - 799210
+                </p>
+                <p>PH - 0381-2865752/62</p>
+            </div>
+        </div> 
+        <div
+           class="
+               container
+               mx-auto
+               max-w-4xl
+               h-full
+               flex
+               flex-wrap
+               justify-between
+               items-center
+               text-sm
+               py-6
+           "
+        >
+            &copy;2023 Designed and Developed by ALGORAC.
+            <div class="pt-4 md:p-0 text-center md:text-right text-xs">
+
+            
+            </div> 
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: "Footer",
+};
+</script>
