51cdb4093c38438f84a5e01c62526ed38eae6210
cion247 <96902253+cion247@users.noreply.github.com>
2023-04-06 15:44:24 +0530

    Update Footer.vue

--- algorac_vue/src/components/Footer.vue
+++ algorac_vue/src/components/Footer.vue
@@ -23,7 +23,7 @@
                         <div class="mb-6 flex justify-center">
                             <a
                             href="https://instagram.com/algorac.iut"
-                            type="button"
+                            type="button" target=”_blank”
                             class="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
                             data-te-ripple-init
                             data-te-ripple-color="light">
@@ -39,7 +39,7 @@
 
                            <a
                             href="https://chat.whatsapp.com/KvpzS7uLBVYGITiNyifsPm"
-                            type="button"
+                            type="button" target=”_blank”
                             class="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
                             data-te-ripple-init
                             data-te-ripple-color="light">
@@ -56,7 +56,7 @@
 
                            <a
                             href="https://github.com/cion247/algorac"
-                            type="button"
+                            type="button" target=”_blank”
                             class="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
                             data-te-ripple-init
                             data-te-ripple-color="light">
