f178a566d2b98f5762d79b44ce79b4fecdd7284a
cion247 <xocionrocks247@gmail.com>
2023-04-07 20:27:42 +0530

    07-04-23

--- algorac_vue/src/components/NavBar.vue
+++ algorac_vue/src/components/NavBar.vue
@@ -1,39 +1,36 @@
-<template>
-  <div>
-    <header class="text-gray-600 body-font">
-      <div
-        class="container font-bold bg-gray-950 mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+<template >
+  <header
+    class="sticky top-0 text-gray-50 z-40 backdrop-blur drop-shadow-lg container mx-auto border-transparent border-8 flex flex-row p-5"
+  >
+    <div
+      class="flex flex-wrap p-5 flex-row items-center justify-items-center w-32 rounded-lg"
+    >
+      <a
+        href="/"
+        class="flex title-font font-medium items-center text-gray-900 mb-4 mx-auto"
       >
-        <a
-          class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        <span class="text-4xl font-black">ALGORAC</span>
+      </a>
+    </div>
+    <div
+      class="container bg-sky-800 flex flex-wrap p-5 ml-auto flex-row items-center w-1/5 lg:w-96 rounded-lg"
+    >
+      <nav
+        class="md:ml-auto flex flex-wrap place-items-start text-xl font-medium justify-start"
+      >
+        <a href="/about" class="m-2 hover:underline underline-offset-8"
+          >about</a
         >
-          <span class="ml-3 font-bold text-slate-100 text-2xl cursor-default">ALGORAC</span>
-        </a>
-        <nav
-          class="md:ml-auto flex flex-wrap items-center text-base justify-center"
-        >
-          <a class="mr-5 text-slate-100 cursor-pointer hover:text-gray-900">Projects</a>
-          <a class="mr-5 text-slate-100 cursor-pointer hover:text-gray-900">Mentors</a>
-        </nav>
-        <button
-          class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
-        >
-          Login
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="w-4 h-4 ml-1"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
-      </div>
-    </header>
-  </div>
+        <a class="m-2 hover:underline underline-offset-8">Projects</a>
+        <a class="m-2 hover:underline underline-offset-8">Mentors</a>
+      </nav>
+      <button
+        class="inline-flex items-center bg-gray-100 text-gray-900 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+      >
+        Login
+      </button>
+    </div>
+  </header>
 </template>
 <script>
 export default {
