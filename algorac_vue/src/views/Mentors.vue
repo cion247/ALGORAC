9cfda29762acda64c2a47c9655bd30a472d2e098
cion247 <xocionrocks247@gmail.com>
2023-04-08 23:00:45 +0530

    d

--- algorac_vue/src/views/Mentors.vue
+++ algorac_vue/src/views/Mentors.vue
@@ -70,6 +70,52 @@
           <p class="text-base text-gray-400 font-normal">Dev Ops</p>
         </div>
       </div>
+      <div
+        class="w-full bg-white bg-opacity-40 backdrop-blur m-4 rounded-lg p-12 flex flex-col justify-center items-center"
+      >
+        <div class="mb-8">
+          <img
+            class="object-center object-cover rounded-full h-36 w-36"
+            src="http://2.bp.blogspot.com/_lwBPBFdTaWM/TIeBFJY4EVI/AAAAAAAAAA8/hta0nczcYrk/s1600/canstockphoto1799922+teacher.jpg"
+            alt="photo"
+          />
+        </div>
+        <div class="text-center">
+          <p class="text-l text-gray-700 font-bold mb-2">NAME</p>
+          <h3>designation</h3>
+          <p class="text-base text-gray-400 font-normal">CONTACT</p>
+        </div>
+      </div>
+      <div
+        class="w-full bg-white bg-opacity-40 backdrop-blur m-4 rounded-lg p-12 flex flex-col justify-center items-center"
+      >
+        <div class="mb-8">
+          <img
+            class="object-center object-cover rounded-full h-36 w-36"
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+            alt="photo"
+          />
+        </div>
+        <div class="text-center">
+          <p class="text-xl text-gray-700 font-bold mb-2">Lucy Carter</p>
+          <p class="text-base text-gray-400 font-normal">Graphic Designer</p>
+        </div>
+      </div>
+      <div
+        class="w-full bg-white bg-opacity-40 backdrop-blur m-4 rounded-lg p-12 flex flex-col justify-center items-center"
+      >
+        <div class="mb-8">
+          <img
+            class="object-center object-cover rounded-full h-36 w-36"
+            src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1176&q=80"
+            alt="photo"
+          />
+        </div>
+        <div class="text-center">
+          <p class="text-xl text-gray-700 font-bold mb-2">Jade Bradley</p>
+          <p class="text-base text-gray-400 font-normal">Dev Ops</p>
+        </div>
+      </div>
     </div>
     <Footer />
   </section>
