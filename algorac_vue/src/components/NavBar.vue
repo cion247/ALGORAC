e091ef8a98e92fa98191a5a3876d30e4e94682a1
cion247 <xocionrocks247@gmail.com>
2023-04-08 17:52:07 +0530

    8423 5

--- algorac_vue/src/components/NavBar.vue
+++ algorac_vue/src/components/NavBar.vue
@@ -1,12 +1,15 @@
 <template >
   <header
-    class="sticky top-0 text-gray-50 z-40 backdrop-blur drop-shadow-lg container mx-auto border-transparent border-8 flex flex-row p-5"
-    >
+    class="sticky top-0 text-gray-900 z-10 backdrop-blur-none drop-shadow container mx-auto border-transparent border-8 flex flex-row p-3"
+  >
     <div
-     class="flex flex-wrap p-5 flex-row items-center justify-items-center w-32 rounded-lg"
-     >
-      <a href="/" class="flex title-font font-medium items-center text-gray-900 mb-4 mx-auto">
-    </a>
+      class="flex flex-wrap p-5 flex-row items-center justify-items-center w-32 rounded-lg"
+    >
+      <a
+        href="/"
+        class="flex title-font font-medium items-center text-gray-900 mb-4 mx-auto"
+      >
+      </a>
     </div>
     <div
       class="flex flex-wrap p-5 flex-row items-center justify-items-center w-32 rounded-lg"
@@ -21,41 +24,39 @@
       <nav
         class="md:ml-auto flex flex-wrap place-items-start text-xl font-bold justify-start"
       >
-        <a href="/about" class="mr-5 hover:text-gray-900">
-          About
-        </a>
+        <a href="/about" class="mr-5 hover:text-gray-900"> About </a>
       </nav>
-        <nav
-          class="md:ml-auto flex flex-wrap items-center text-base justify-center"
+      <nav
+        class="md:ml-auto flex flex-wrap items-center text-base justify-center"
+      >
+        <a href="/projects" target="_blank" class="mr-5 hover:text-gray-900"
+          >Projects</a
         >
-          <a href="/projects" target="_blank" class="mr-5 hover:text-gray-900">Projects</a>
-          <a class="mr-5 hover:text-gray-900">Mentors</a>
-        </nav>
-        <button
-          class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-black mt-4 md:mt-0"
+        <a class="mr-5 hover:text-gray-900">Mentors</a>
+      </nav>
+      <button
+        class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-black mt-4 md:mt-0"
+      >
+        <a href="/login">Login</a>
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-4 h-4 ml-1"
+          viewBox="0 0 24 24"
         >
-          <a href="/login">Login</a>
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="w-4 h-4 ml-1"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
+          <path d="M5 12h14M12 5l7 7-7 7"></path>
+        </svg>
+      </button>
     </div>
-  </header> 
+  </header>
 </template>
-<script> 
+<script>
 export default {
   name: "NavBar",
 };
-
-
 </script>
     
     
