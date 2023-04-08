dbc2c096117360d25f568c697c17022cba0e3d51
cion247 <xocionrocks247@gmail.com>
2023-04-08 09:55:10 +0530

    8423 3

--- algorac_vue/src/components/AboutUs.vue
+++ algorac_vue/src/components/AboutUs.vue
@@ -1,53 +1,51 @@
 <template>
-  <div class="hello">
-    <div
-      class="container shadow-2xl border-6 rounded-lg mx-auto my-20 w-1/3 border border-orange-500 bg-white"
-    >
-      <div class="p-5 space-y-5 shadow-xl">
-        <h4 class="font-serif text-center text-3xl">Send Queries</h4>
+  <div
+    class="container shadow-2xl border-6 rounded-lg mx-auto my-20 w-1/3 border border-orange-500 bg-white"
+  >
+    <div class="p-5 space-y-5 shadow-xl">
+      <h4 class="font-serif text-center text-3xl">Send Queries</h4>
 
-        <form>
-          <div class="grid grid-cols-2 gap-5">
-            <input
-              type="text"
-              class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500"
-              placeholder="First Name"
-            />
-            <input
-              type="text"
-              class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500"
-              placeholder="Last Name"
-            />
-            <input
-              type="email"
-              class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
-              placeholder="Email"
-            />
-            <textarea
-              cols="10"
-              rows="5"
-              class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
-              placeholder="Write your message..."
-            ></textarea>
-            <div class="mt-15">
-              <input type="checkbox" class="border border-gray-400" />
-              <span>
-                I accept the
-                <a href="#" class="text-green-500 font-semibold">Term of Use</a>
-                &
-                <a href="#" class="text-green-500 font-semibold"
-                  >Private Policy</a
-                ></span
-              >
-            </div>
-          </div>
+      <form>
+        <div class="grid grid-cols-2 gap-5">
           <input
-            type="submit"
-            value="Send Message"
-            class="cursor-pointer focus:outline-none mt-5 bg-orange-500 px-4 py-2 text-white font-bold w-full"
+            type="text"
+            class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500"
+            placeholder="First Name"
           />
-        </form>
-      </div>
+          <input
+            type="text"
+            class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500"
+            placeholder="Last Name"
+          />
+          <input
+            type="email"
+            class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
+            placeholder="Email"
+          />
+          <textarea
+            cols="10"
+            rows="5"
+            class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
+            placeholder="Write your message..."
+          ></textarea>
+          <div class="mt-15">
+            <input type="checkbox" class="border border-gray-400" />
+            <span>
+              I accept the
+              <a href="#" class="text-green-500 font-semibold">Term of Use</a>
+              &
+              <a href="#" class="text-green-500 font-semibold"
+                >Private Policy</a
+              ></span
+            >
+          </div>
+        </div>
+        <input
+          type="submit"
+          value="Send Message"
+          class="cursor-pointer focus:outline-none mt-5 bg-orange-500 px-4 py-2 text-white font-bold w-full"
+        />
+      </form>
     </div>
   </div>
 </template>
