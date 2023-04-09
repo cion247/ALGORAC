e33007275141ac0ae567d3ba308bc7f63d589a3b
cion247 <xocionrocks247@gmail.com>
2023-04-09 20:23:50 +0530

    09-04-23

--- algorac_vue/src/components/NoticeBoard.vue
+++ algorac_vue/src/components/NoticeBoard.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 bg-slate-200 rounded-lg my-8 h-96 border-1 text-stone-900 p-2 overflow-auto"
+    class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 bg-slate-200 rounded my-8 h-96 border-1 text-stone-900 p-2 overflow-auto"
   >
     <h1
-      class="title-font sm:text-4xl text-3xl mb-1 font-normal text-stone-900 text-center rounded-xl p-1"
+      class="title-font sm:text-4xl text-3xl mb-1 font-normal text-stone-900 text-center p-1"
     >
       notice_board
     </h1>
@@ -15,7 +15,7 @@
         v-for="notice in this.latestNotices"
         v-bind:key="notice.id"
       >
-        <div class="p-1 rounded-lg flex flex-col text-lg break-words grow">
+        <div class="p-1 flex flex-col text-lg break-words grow">
           <h1 class="font-bold">{{ notice.topic }}</h1>
           <h1>{{ notice.description }}</h1>
           <h1>------------------------------</h1>
--- algorac_vue/tailwind.config.js
+++ algorac_vue/tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+
     extend: {
       backgroundImage: {
         'one': "url('/src/assets/bgpic.svg')"
