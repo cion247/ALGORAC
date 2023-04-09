c60f792061165054631a1bd53a141c97f0964de2
RANADIP GOPE <129092566+Ranadip213@users.noreply.github.com>
2023-04-09 18:55:22 +0530

    contact form text colour

--- algorac_vue/src/components/AboutUs.vue
+++ algorac_vue/src/components/AboutUs.vue
@@ -25,55 +25,35 @@
             quantity and quality of programmers irrespective of their respective
             engineering discipline.
           </p>
-          <a
-            class="bg-gray-500 text-white py-4 px-12 rounded-full hover:bg-white-600"
-            ><i>ALGORAC believes in coding the problems away.</i></a
-          >
+          <a class="bg-gray-500 text-white py-4 px-12 rounded-full hover:bg-white-600"><i>ALGORAC believes in coding the
+              problems away.</i></a>
         </div>
         <div class="w-1/2 px-2">
-          <div
-            class="shadow-2xl border-1 mx-auto my-20 border rounded-lg backdrop-blur-lg"
-          >
+          <div class="shadow-2xl border-1 mx-auto my-20 border rounded-lg backdrop-blur-lg">
             <div class="p-5 space-y-5 shadow-xl">
               <h4 class="font-serif text-center text-3xl">Send Queries</h4>
 
               <form>
-                <div class="grid grid-cols-2 gap-5">
-                  <input
-                    type="text"
+                <div class="grid text-gray-900 grid-cols-2 gap-5">
+                  <input type="text"
                     class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
-                    placeholder="Full Name"
-                  />
-                  <input
-                    type="email"
+                    placeholder="Full Name" />
+                  <input type="email"
                     class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
-                    placeholder="Email"
-                  />
-                  <textarea
-                    cols="10"
-                    rows="5"
+                    placeholder="Email" />
+                  <textarea cols="10" rows="5"
                     class="border border-gray-500 px-4 py-2 focus:outline-none focus:border-purple-500 col-span-2"
-                    placeholder="Write your message..."
-                  ></textarea>
-                  <div class="mt-15">
+                    placeholder="Write your message..."></textarea>
+                  <div class="mt-15 text-gray-200">
                     <input type="checkbox" class="border border-gray-400" />
                     <span>
                       I accept the
-                      <a href="#" class="text-green-500 font-semibold"
-                        >Term of Use</a
-                      >
-                      &
-                      <a href="#" class="text-green-500 font-semibold"
-                        >Private Policy</a
-                      ></span
-                    >
+                      <a href="#" class="text-green-500 font-semibold">Term of Use</a>
+                      &<a href="#" class="text-green-500 font-semibold">Private Policy</a></span>
                   </div>
                 </div>
-                <input
-                  type="submit"
-                  value="Send Message"
-                  class="cursor-pointer focus:outline-none mt-5 bg-orange-500 px-4 py-2 text-white font-bold w-full"
-                />
+                <input type="submit" value="Send Message"
+                  class="cursor-pointer focus:outline-none mt-5 bg-sky-900 px-4 py-2 text-white font-bold w-full" />
               </form>
             </div>
           </div>
@@ -86,7 +66,7 @@
 <script>
 export default {
   name: "AboutUs",
-  data() {},
+  data() { },
   components: {},
 };
 </script>
