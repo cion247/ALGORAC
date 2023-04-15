5766e389daefd423e41809ba9c9156c170240ad9
joydip <joydip@pop-os.localdomain>
2023-04-15 20:32:15 +0530

    shure

--- AR_django/base/urls.py
+++ AR_django/base/urls.py
@@ -1,4 +1,6 @@
+from django.conf.urls.static import static
 from django.urls import path, include
+from django.conf import settings
 
 from base.views import LatestNoticelistView, MentorView, MyTokenObtainPairView, NoticedetailView, LatestGallerylistView, ProjectView, MessagesView, FeadbackView, UserCreateAPIView
 from rest_framework_simplejwt.views import (
@@ -23,6 +25,6 @@ urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/create/', UserCreateAPIView.as_view(), name='user_create')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
