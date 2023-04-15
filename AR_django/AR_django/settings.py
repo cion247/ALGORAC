1e6097a13d50836791b87fa31062e8d7afe47b4d
joydip <joydip@pop-os.localdomain>
2023-04-15 19:18:06 +0530

    url added

--- AR_django/AR_django/settings.py
+++ AR_django/AR_django/settings.py
@@ -29,6 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = ['https://backenddjango-production.up.railway.app',
+                        'https://www.backenddjango-production.up.railway.app']
+
 
 
 # Application definition
